73f497bf004cdedb7820340f0abf5d4ee4232ef2
Chequemeout <jackexyoU@gmail.com>
2016-04-15 08:26:15 -0400

    Add todo route

---
+++ app/routes/todos.js
@@ -0,0 +1,15 @@
+// app/routes/todos.js
+
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+
+  model() {
+    return [
+      { text: "Create Ember app" },
+      { text: "Read Ember Igniter" },
+      { text: "Master Ember" }
+    ]
+  }
+
+});
